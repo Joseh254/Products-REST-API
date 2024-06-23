f65483c0224810be39e41a397286915129ff381d
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-23 18:55:01 +0300

    hide sensitive information

--- src/database.configure.js
+++ src/database.configure.js
@@ -1,12 +1,13 @@
 import pkg from 'pg';
+import { config } from 'dotenv';
 const { Pool } = pkg;
-
+config()
 const pool = new Pool({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'products'
+  user: process.env.PG_USER,
+  password:  process.env.PG_PASSWORD,
+  host:  process.env.PG_HOST,
+  port:  process.env.PG_PORT,
+  database:  process.env.PG_DATABASE
 });
 
 export default pool;
